d2adc9976f939726aed9d8669bffdbf6c7182b20
Bashar Ahmad Khan <150275763+Basharkhan243@users.noreply.github.com>
2025-07-19 14:23:56 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Homepage() {
   const [darkMode, setDarkMode] = useState(false);
+   const navigate = useNavigate();
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
