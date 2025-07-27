d4b117d45dcf554387ef1294ff134f858488f2a1
Bashar Ahmad Khan <150275763+Basharkhan243@users.noreply.github.com>
2025-07-27 15:35:26 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,6 +10,7 @@ export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
